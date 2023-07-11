98e5f5713e09ad15c0117f4df526c9d716d29b33
Olga Miasnikova <omyasn91@gmail.com>
2023-07-12 00:00:42 +0200

    increase length

--- src/pages/SplitsPage/components/ItemsLayout/ItemsLayout.tsx
+++ src/pages/SplitsPage/components/ItemsLayout/ItemsLayout.tsx
@@ -28,7 +28,7 @@ function getItemName(name: string, characters: Character[]) {
 
 function isTooLong(name: string, characters: Character[]) {
   const length = name.length + characters.reduce((acc, curr, currIndex) => currIndex < 3 ? acc + curr.name.length : acc, 0);
-  return length > 53;
+  return length > 55;
 }
 
 function getShortItem (name:string) {
