61f7f43f68582f62be31d028e2ea69a38683a2d8
Olga Miasnikova <omyasn91@gmail.com>
2023-07-11 21:40:48 +0200

    short page

--- src/pages/SplitsPage/components/ItemsLayout/ItemsLayout.tsx
+++ src/pages/SplitsPage/components/ItemsLayout/ItemsLayout.tsx
@@ -1,6 +1,7 @@
-import { ItemCharactersMap } from 'src/types';
+import { ItemCharactersMap, Character } from 'src/types';
 import styles from '../RaidLayout/raidLayout.module.css';
 import { CharactersList } from './CharactersList';
+import { useState } from 'react';
 
 export const ItemsLayout = ({ items }: { items: ItemCharactersMap }) => {
   const itemsNames = Object.keys(items);
@@ -11,12 +12,40 @@ export const ItemsLayout = ({ items }: { items: ItemCharactersMap }) => {
       <p className="font-semibold">Items</p>
 
       {sortedItemsNames.map(item => (
-        <div className="text-left mb-1 text-sm" key={item}>
-          <div>
-            {item} ({items[item].length}): <CharactersList characters={items[item]} />
-          </div>
+        <div className="text-left mb-px text-sm" key={item}>
+            {getItemName(item, items[item])}<span className='text-[12px] align-super'>{items[item].length}</span>: <CharactersList characters={items[item]} />
         </div>
       ))}
     </div>
   );
 };
+
+function getItemName(name: string, characters: Character[]) {
+  return isTooLong(name, characters) ?  <ShortName name={name} />: name;
+}
+
+function isTooLong(name: string, characters: Character[]) {
+  const length = name.length + characters.reduce((acc, curr, currIndex) => currIndex < 3 ? acc + curr.name.length : acc, 0);
+  return length > 54;
+}
+
+function getShortItem (name:string) {
+  const secWords = ['of', 'the', 'with', 'and'];
+  return  name
+    .split(' ')
+    .map((part) => {
+      if (secWords.includes(part)) {
+        return part[0].toLowerCase();
+      }
+      return part[0].toUpperCase();
+    })
+    .join('');
+}
+
+const ShortName = ({ name }: { name: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <span className='cursor-pointer' onClick={() => setIsOpen(prevState => !prevState)}>{isOpen ? name : getShortItem(name)}</span>
+  );
+}
